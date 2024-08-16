dcff1fe66694cf1dc0e1200c10c83528225d81b3
Mateo Buitrago <matbui.mbt@gmail.com>
2024-08-15 20:13:03 -0500

    first api call

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -1,8 +1,22 @@
+import fetch from 'node-fetch';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    const url =
+      'https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1';
+
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${process.env.AUTH_TOKEN}`,
+      },
+    };
+
+    return fetch(url, options)
+      .then((res) => res.json())
+      .catch((err) => console.error('error:' + err));
   }
 }
